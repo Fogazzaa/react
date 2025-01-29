9303a1ee8192a675434e6f4b667ec1b74b14adb9
fogazza <vinicius.cintra2801@gmail.com>
2025-01-29 11:22:06 -0300

    29-01-25

---
+++ meuSiteBasico/src/App.jsx
@@ -0,0 +1,11 @@
+import Component1 from "./Component.jsx";
+
+function Fodasi() {
+  return (
+    <div>
+      <Component1 />
+    </div>
+  );
+}
+
+export default Fodasi;
---
+++ meuSiteButton/src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div>
+      <h1>Bem vindo ao React</h1>
+      <p>Exemplo 2 de componentização</p>
+      <Button></Button>
+    </div>
+  );
+}
+
+function Button() {
+  function click() {
+    alert("voce clicou no botão");
+  }
+  return (
+    <div>
+      <button
+        style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
+        onClick={()=>click()}
+      >
+        Clique aqui
+      </button>
+    </div>
+  );
+}
+
+export default App;
