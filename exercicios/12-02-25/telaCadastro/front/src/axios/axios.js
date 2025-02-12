71a198c9fa46d9b496050a564a21aa4fdf220a6f
fogazza <vinicius.cintra2801@gmail.com>
2025-02-12 09:27:09 -0300

    12-02-25

--- exercicios/12-02-25/telaCadastro/front/src/axios/axios.js
+++ exercicios/12-02-25/telaCadastro/front/src/axios/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:
+    baseURL:"http://localhost:5000/api/v1/",
+    headers:{'accept':'application/json'}
 })
+
+const sheets = {
+    postLogin:(user) => api.post("login/", user)
+}
