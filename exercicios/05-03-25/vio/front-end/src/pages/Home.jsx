a577cdf17c1d0a89057ac7a2dd795a1358a9ca1d
fogazza <vinicius.cintra2801@gmail.com>
2025-03-05 11:01:16 -0300

    05-03-25

--- exercicios/05-03-25/vio/front-end/src/pages/Home.jsx
+++ exercicios/05-03-25/vio/front-end/src/pages/Home.jsx
@@ -1,14 +1,29 @@
 
 import { Link } from "react-router-dom";
 
+import Button from "@mui/material/Button";
+
 function Home() {
 
   return (
     <div>
-      <Link to="/eventos">Lista de Eventos</Link>
-      <Link to="/organizadores">Lista de Organizadores</Link>
-      <Link to="/ingressos">Lista de Ingressos</Link>
-      <Link to="/evento/novo">Criar Evento</Link>
+      <h1>Home</h1>
+      <h3>Lista de Eventos</h3>
+      <Button  variant="contained" component={Link} to="/eventos">
+        Lista de Eventos
+      </Button>
+      <h3>Lista de Organizadores</h3>
+      <Button  variant="contained" component={Link} to="/organizadores">
+        Lista de Organizadores
+      </Button>
+      <h3>Lista de Ingressos</h3>
+      <Button  variant="contained" component={Link} to="/ingressos">
+        Lista de Ingressos
+      </Button>
+      <h3>Crie Eventos</h3>
+      <Button  variant="contained" component={Link} to="/evento/novo">
+        Crie Eventos
+      </Button>
     </div>
   );
 }
